30599038d874c186c0f1036424e88570da5aba49
Stephen Moss <smoss@udel.edu>
2020-11-08 17:13:48 -0500

    layout kind of done, all buttons do the same, now needs behavior

--- NeoDemo/src/main/java/BlankSandbox.java
+++ NeoDemo/src/main/java/BlankSandbox.java
@@ -29,7 +29,7 @@ public class BlankSandbox {
 	   
     public static int maxDisplayed;
     
-	public static JTextArea l;
+
 	public static JTextArea exceptionDisplay;
 	public static JTextArea queryInput;
 	
@@ -40,6 +40,16 @@ public class BlankSandbox {
     	//Create JFrames
     	JFrame overallJFrame = new JFrame("Movies Database Queries");
     	overallJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    	
+    	//Create buttons
+    	JButton createButton = new JButton("Create");
+    	JButton readButton = new JButton("Read");
+    	JButton updateButton = new JButton("Update");
+    	JButton destroyButton = new JButton("Destroy");
+    	
+    	JButton queryExecuteButton = new JButton("Arbitrary Query");   
+    	
     	
     	//Create and setup TextAreas
     	final JTextArea resultsArea = new JTextArea(20,40);
@@ -50,9 +60,16 @@ public class BlankSandbox {
     	exceptionDisplay.setEditable(false);
     	exceptionDisplay.setText("Any Exceptions Here");
     	
+    	//(Inputs for queries)
     	final JTextArea queryInput = new JTextArea(1,40);
     	
-    	//Put TextAreas in created Scrollpanes, setup scrollpanes
+    	final JTextArea createInput = new JTextArea(1,20);
+    	final JTextArea readInput = new JTextArea(1,20);
+    	final JTextArea updateInput = new JTextArea(1,20);
+    	final JTextArea destroyInput = new JTextArea(1,20);
+    	
+    	
+    	//Put TextAreas in created panes, setup panes
     	final JScrollPane resultsScrollPane = new JScrollPane(resultsArea);
     	final JScrollPane exceptionScrollPane = new JScrollPane(exceptionDisplay);
     	
@@ -60,23 +77,46 @@ public class BlankSandbox {
     	resultsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
     	exceptionScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
     	
-    	//Create buttons
-    	JButton readButton = new JButton("Read");
-    	JButton queryExecuteButton = new JButton("Arbitrary Query");
     	
     	//Create and setup Jpanel
-    	JPanel guiPanel = new JPanel();
-    	guiPanel.add(readButton);
-    	JPanel resultsPanel = new JPanel();
-    	JPanel exceptionPanel = new JPanel();
     	JPanel queryPanel = new JPanel();
     	queryPanel.add(queryInput);
-    	queryPanel.add(queryExecuteButton);
+    	queryPanel.add(queryExecuteButton);   
+   
+    	JPanel guiPanel = new JPanel();
+    	JPanel createPanel = new JPanel();
+    	createPanel.add(createInput);
+    	createPanel.add(createButton);
+    	
+    	JPanel readPanel = new JPanel();
+    	readPanel.add(readInput);
+    	readPanel.add(readButton);
+    	
+    	JPanel updatePanel = new JPanel();
+    	updatePanel.add(updateInput);
+    	updatePanel.add(updateButton);
+    	
+    	JPanel destroyPanel = new JPanel();
+    	destroyPanel.add(destroyInput);
+    	destroyPanel.add(destroyButton);
+    	
+    	JPanel resultsPanel = new JPanel();
+    	JPanel exceptionPanel = new JPanel();
+    	
+    	
+    	//Set up tabbed pane for gui
+    	JTabbedPane guiPane = new JTabbedPane();
+    	guiPane.addTab("CREATE", createPanel);
+    	guiPane.addTab("READ", readPanel);
+    	guiPane.addTab("UPDATE", updatePanel);
+    	guiPane.addTab("DESTROY", destroyPanel);
+    	
     	
     	//Split panes
     	JSplitPane resultsSplitPane = new JSplitPane(SwingConstants.HORIZONTAL, resultsScrollPane, exceptionScrollPane);
-    	JSplitPane topOverallSplitPane = new JSplitPane(SwingConstants.VERTICAL, guiPanel, resultsSplitPane);
+    	JSplitPane topOverallSplitPane = new JSplitPane(SwingConstants.VERTICAL, guiPane, resultsSplitPane);
     	JSplitPane overallPane = new JSplitPane(SwingConstants.HORIZONTAL, topOverallSplitPane, queryPanel);
+    	
     	
     	//Add components to panels
     	resultsPanel.add(overallPane);
@@ -90,8 +130,160 @@ public class BlankSandbox {
     	
     	maxDisplayed = 50;
     	
-    	//
+    	//SET UP THE LISTENERS AND THEIR ASSOCIATED QUERIES
+        createButton.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ArrayList resultList = new ArrayList();
+				String results = "";	
+				int numDisplayed = 0;
+		    	try (Session session = driver.session()) {
+		            String cypherQuery = "Match (m:Movie) RETURN m.title";
+		            StatementResult result = session.run(cypherQuery, parameters());
+	            	
+		            //While results remain, add them to a list
+		            while (result.hasNext()) {
+		                resultList.add( result.next().get("m.title") + "");
+		                System.out.println(results);
+		            }
+		            
+		            //Sort the list
+		            Collections.sort(resultList);
+		            
+		            //for each in the list, add it to the result string
+		            for (Object title : resultList) {
+		            	results += title.toString() + "\r\n";
+		            }
+		          
+		            //display the result string
+		            resultsArea.setText(results);
+		            System.out.print(results);
+		            
+		        }
+		    	
+		    	catch(Exception exception){
+		    		errorDisplay(exception, exceptionDisplay);
+		    	}
+		    	
+			}
+        });
+    	
         readButton.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ArrayList resultList = new ArrayList();
+				String results = "";	
+				int numDisplayed = 0;
+		    	try (Session session = driver.session()) {
+		            String cypherQuery = "Match (m:Movie) RETURN m.title";
+		            StatementResult result = session.run(cypherQuery, parameters());
+	            	
+		            //While results remain, add them to a list
+		            while (result.hasNext()) {
+		                resultList.add( result.next().get("m.title") + "");
+		                System.out.println(results);
+		            }
+		            
+		            //Sort the list
+		            Collections.sort(resultList);
+		            
+		            //for each in the list, add it to the result string
+		            for (Object title : resultList) {
+		            	results += title.toString() + "\r\n";
+		            }
+		          
+		            //display the result string
+		            resultsArea.setText(results);
+		            System.out.print(results);
+		            
+		        }
+		    	
+		    	catch(Exception exception){
+		    		errorDisplay(exception, exceptionDisplay);
+		    	}
+		    	
+			}
+        });
+        
+        updateButton.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ArrayList resultList = new ArrayList();
+				String results = "";	
+				int numDisplayed = 0;
+		    	try (Session session = driver.session()) {
+		            String cypherQuery = "Match (m:Movie) RETURN m.title";
+		            StatementResult result = session.run(cypherQuery, parameters());
+	            	
+		            //While results remain, add them to a list
+		            while (result.hasNext()) {
+		                resultList.add( result.next().get("m.title") + "");
+		                System.out.println(results);
+		            }
+		            
+		            //Sort the list
+		            Collections.sort(resultList);
+		            
+		            //for each in the list, add it to the result string
+		            for (Object title : resultList) {
+		            	results += title.toString() + "\r\n";
+		            }
+		          
+		            //display the result string
+		            resultsArea.setText(results);
+		            System.out.print(results);
+		            
+		        }
+		    	
+		    	catch(Exception exception){
+		    		errorDisplay(exception, exceptionDisplay);
+		    	}
+		    	
+			}
+        });
+        
+        destroyButton.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ArrayList resultList = new ArrayList();
+				String results = "";	
+				int numDisplayed = 0;
+		    	try (Session session = driver.session()) {
+		            String cypherQuery = "Match (m:Movie) RETURN m.title";
+		            StatementResult result = session.run(cypherQuery, parameters());
+	            	
+		            //While results remain, add them to a list
+		            while (result.hasNext()) {
+		                resultList.add( result.next().get("m.title") + "");
+		                System.out.println(results);
+		            }
+		            
+		            //Sort the list
+		            Collections.sort(resultList);
+		            
+		            //for each in the list, add it to the result string
+		            for (Object title : resultList) {
+		            	results += title.toString() + "\r\n";
+		            }
+		          
+		            //display the result string
+		            resultsArea.setText(results);
+		            System.out.print(results);
+		            
+		        }
+		    	
+		    	catch(Exception exception){
+		    		errorDisplay(exception, exceptionDisplay);
+		    	}
+		    	
+			}
+        });
+        
+        queryExecuteButton.addActionListener(new ActionListener() {
         	
 			@Override
 			public void actionPerformed(ActionEvent e) {
