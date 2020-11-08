8e94ae5ccebfbfc45996419e9afebfee9f604099
Stephen Moss <smoss@udel.edu>
2020-11-08 15:39:08 -0500

    On the way to formatting

--- NeoDemo/src/main/java/BlankSandbox.java
+++ NeoDemo/src/main/java/BlankSandbox.java
@@ -7,6 +7,8 @@ import static org.neo4j.driver.v1.Values.parameters;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.Collections;
 import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonMap;
@@ -22,45 +24,101 @@ public class BlankSandbox {
 	 */
 	//BEGIN DB INFO
     public static Config noSSL = Config.build().withEncryptionLevel(Config.EncryptionLevel.NONE).toConfig();
-    public static Driver driver = GraphDatabase.driver("bolt://100.25.221.22:42385",AuthTokens.basic("neo4j","chance-effect-huts"),noSSL); // <password>
+    public static Driver driver = GraphDatabase.driver("bolt://52.201.184.95:33664",AuthTokens.basic("neo4j","dockings-module-services"),noSSL); // <password>
 	//END DB INFO
-	
-	public static JLabel l;
+	   
+    public static int maxDisplayed;
+    
+	public static JTextArea l;
+	public static JTextArea exceptionDisplay;
 	
     public static void main(String...args) {
     	
-    	JFrame a = new JFrame("Basic Example");
-    	a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	//Set everything up, initialize
+    	
+    	//Create JFrames
+    	JFrame jframeCreate = new JFrame("Basic Example");
+    	jframeCreate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	//Create TextAreas
+    	
+    	final JTextArea l = new JTextArea(20,40);
+    	final JTextArea exceptionDisplay = new JTextArea(10,20);
+    	
+    	//Put TextAreas in Scrollpanes
+    	
+    	
     	JButton b = new JButton("Get Results");
     	JPanel p = new JPanel(); 
-    	final JLabel l = new JLabel();
-    	l.setText("Query Results Here");
-    	b.setBounds(40,90,85,20);
-    	p.add(l);
-    	p.add(b);
-    	a.add(p);
-    	a.setSize(300,300);
-    	a.setVisible(true);
     	
+    	l.setEditable(false);
+    	
+    	final JScrollPane resultsScrollPlane = new JScrollPane(l);
+    	resultsScrollPlane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    	l.setText("Query Results Here");
+    	//b.setBounds(40,90,85,20);
+    	p.add(resultsScrollPlane);
+    	p.add(exceptionDisplay);
+    	p.add(b);
+    	jframeCreate.add(p);
+    	jframeCreate.setSize(1000,1000);
+    	jframeCreate.setVisible(true);
+    	
+    	
+    	maxDisplayed = 50;
+    	
+    	//
         b.addActionListener(new ActionListener() {
         	
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String results = "";			
+				ArrayList resultList = new ArrayList();
+				String results = "";	
+				int numDisplayed = 0;
 		    	try (Session session = driver.session()) {
 		            String cypherQuery = "Match (m:Movie) RETURN m.title";
 		            StatementResult result = session.run(cypherQuery, parameters());
+	            	
+		            //While results remain, add them to a list
 		            while (result.hasNext()) {
-		                results += result.next().get("m.title") + " \r\n";
+		                resultList.add( result.next().get("m.title") + "");
+		                System.out.println(results);
 		            }
+		            
+		            //Sort the list
+		            Collections.sort(resultList);
+		            
+		            //for each in the list, add it to the result string
+		            for (Object title : resultList) {
+		            	results += title.toString() + "\r\n";
+		            }
+		          
+		            //display the result string
+		            l.setText(results);
+		            System.out.print(results);
+		            
 		        }
-		    	l.setText(results);
-		    	System.out.println(results);
+		    	
+		    	/*
+		    	catch(Exception exception){
+		    		errorDisplay(exception, l);
+		    	}
+		    	*/
 			}
         });
        
     }
- 
+    
+    private static void errorDisplay(Exception exception, JTextArea textarea) {
+    	
+    	String results = "";
+    	results += "Error retrieving\r\n";
+		results += exception.toString();
+		textarea.setText(results);
+		System.out.println(results);
+		
+    }
     
 }
 
